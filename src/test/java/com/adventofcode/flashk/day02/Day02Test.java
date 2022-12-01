85d8ff5e4067582f4578bb83ab050ef5d1784f88
Flashky <mevengar@gmail.com>
2022-12-01 16:39:13 +0100

    Remove disabled tests for day 02

--- src/test/java/com/adventofcode/flashk/day02/Day02Test.java
+++ src/test/java/com/adventofcode/flashk/day02/Day02Test.java
@@ -21,7 +21,6 @@ import com.adventofcode.flashk.common.test.utils.Util;
 
 @DisplayName(TestDisplayName.DAY_02)
 @TestMethodOrder(OrderAnnotation.class)
-@Disabled // TODO Remove comment when implemented
 public class Day02Test extends PuzzleTest {
 
 	private final static String INPUT_FOLDER = TestFolder.DAY_02;
