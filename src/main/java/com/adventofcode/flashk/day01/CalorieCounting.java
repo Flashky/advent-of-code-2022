da2f7e19ddd1875518a46b076bd23d9011a62623
Flashky <mevengar@gmail.com>
2022-12-01 07:34:32 +0100

    Refactory day 01 puzzle

--- src/main/java/com/adventofcode/flashk/day01/CalorieCounting.java
+++ src/main/java/com/adventofcode/flashk/day01/CalorieCounting.java
@@ -3,71 +3,38 @@ package com.adventofcode.flashk.day01;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class CalorieCounting {
 
-	public int solve(List<String> calories) {
-		
-		List<Integer> elvesCalories = new ArrayList<>();
-		
-		Integer maxCalories = 0;
-		Integer currentElveCalories = 0;
-		
-		for(String calorie : calories) {
-			if(StringUtils.isBlank(calorie)) {
-				
-				if(currentElveCalories > maxCalories) {
-					maxCalories = currentElveCalories;
-				}
-				
-				// Count total and reset elve
-				elvesCalories.add(currentElveCalories);
-				currentElveCalories = 0;
-				
-			} else {
-				currentElveCalories += Integer.parseInt(calorie);
-			}
-		}
-		
-		elvesCalories.add(currentElveCalories);
-		
-		return maxCalories;
-	}
+	private List<Integer> elvesCalories = new ArrayList<>();
 	
-	public int solveB(List<String> calories) {
-		
-		List<Integer> elvesCalories = new ArrayList<>();
+	public CalorieCounting(List<String> inputs) {
 
-		Integer maxCalories = 0;
 		Integer currentElveCalories = 0;
 		
-		for(String calorie : calories) {
+		for(String calorie : inputs) {
 			if(StringUtils.isBlank(calorie)) {
-				
-				if(currentElveCalories > maxCalories) {
-					maxCalories = currentElveCalories;
-				}
-				
-				// Count total and reset elve
 				elvesCalories.add(currentElveCalories);
 				currentElveCalories = 0;
-				
 			} else {
 				currentElveCalories += Integer.parseInt(calorie);
 			}
 		}
+		
 		elvesCalories.add(currentElveCalories);
-		List<Integer> sortedCalories = elvesCalories.stream().sorted(Collections.reverseOrder()).limit(3).collect(Collectors.toList());
-		
-		
-		return sortedCalories.get(0) + sortedCalories.get(1) + sortedCalories.get(2);
+
 	}
 	
-	/*
-	private List<Integer> calculateElvesCalories(List<String> calories) {
+	public int solve(int numberOfElves) {
 		
-	}*/
+		// Use an AtomicInteger as I need wrapper object in forEach
+		AtomicInteger totalCalories = new AtomicInteger(0);
+		elvesCalories.stream().sorted(Collections.reverseOrder()).limit(numberOfElves).forEach(elveCalories -> totalCalories.getAndAdd(elveCalories));
+		return totalCalories.get();
+		
+	}
+	
 }
--- src/test/java/com/adventofcode/flashk/day01/Day01Test.java
+++ src/test/java/com/adventofcode/flashk/day01/Day01Test.java
@@ -45,8 +45,8 @@ public class Day01Test extends PuzzleTest {
 		// Read input file
 		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE_SAMPLE);
 		
-		CalorieCounting calorieCounting = new CalorieCounting();
-		int result = calorieCounting.solve(inputs);
+		CalorieCounting calorieCounting = new CalorieCounting(inputs);
+		int result = calorieCounting.solve(1);
 		
 		assertEquals(24000, result);
 		
@@ -64,8 +64,8 @@ public class Day01Test extends PuzzleTest {
 		// Read input file
 		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE);
 		
-		CalorieCounting calorieCounting = new CalorieCounting();
-		int result = calorieCounting.solve(inputs);
+		CalorieCounting calorieCounting = new CalorieCounting(inputs);
+		int result = calorieCounting.solve(1);
 		
 		assertEquals(70613, result);
 	}
@@ -82,8 +82,8 @@ public class Day01Test extends PuzzleTest {
 		// Read input file
 		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE_SAMPLE);
 		
-		CalorieCounting calorieCounting = new CalorieCounting();
-		int result = calorieCounting.solveB(inputs);
+		CalorieCounting calorieCounting = new CalorieCounting(inputs);
+		int result = calorieCounting.solve(3);
 		
 		assertEquals(45000, result);
 	}
@@ -100,8 +100,8 @@ public class Day01Test extends PuzzleTest {
 		// Read input file
 		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE);
 		
-		CalorieCounting calorieCounting = new CalorieCounting();
-		int result = calorieCounting.solveB(inputs);
+		CalorieCounting calorieCounting = new CalorieCounting(inputs);
+		int result = calorieCounting.solve(3);
 		
 		assertEquals(205805, result);
 		
