3d8ba0f3b1cd90aeb8051535af6f3abf320b76d5
Flashk <mevengar@gmail.com>
2022-12-07 09:20:27 +0100

    Refactor day 07 code a little bit

--- src/main/java/com/adventofcode/flashk/day07/File.java
+++ src/main/java/com/adventofcode/flashk/day07/File.java
@@ -2,34 +2,61 @@ package com.adventofcode.flashk.day07;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
-public class File {
+public class File implements Comparable<File> {
 
-	private String type; // file/dir
+	private final static String DIR_REGEX = "dir ([a-z]*)";
+	private final static Pattern DIR_PATTERN = Pattern.compile(DIR_REGEX);
+	
+	private final static String FILE_REGEX = "^([0-9]*) ([a-z]*.[a-z]*)";
+	private final static Pattern FILE_PATTERN = Pattern.compile(FILE_REGEX);
+	
+	private boolean isDirectory;
 	private String name;
+	
+	@Setter
 	private long size = 0;
 	private List<File> childFiles;
 	
-	public File(String type, String name, long size) {
-		this.type = type;
+	public File(boolean isDirectory, String name, long size) {
+		this.isDirectory = isDirectory;
 		this.name = name;
 		this.size = size;
 		
-		if(type == "dir") {
+		if(this.isDirectory) {
 			childFiles = new ArrayList<>();
 		}
+	}
+	
+	public File(String input) {
+		
+		Matcher directoryMatcher = DIR_PATTERN.matcher(input);
+		Matcher fileMatcher = FILE_PATTERN.matcher(input);
+		
+		if(directoryMatcher.find()) {
+			isDirectory = true;
+			name = directoryMatcher.group(1);
+			size = 0;
+			childFiles = new ArrayList<>();
+		} else if(fileMatcher.find()) {
+			isDirectory = false;
+			name = fileMatcher.group(2);
+			size = Long.parseLong(fileMatcher.group(1));
+		}
+		
 	}
 
 	public void addChild(File file) {
 		
-		if(this.type != "dir") {
+		if(!isDirectory) {
 			throw new IllegalArgumentException("Current file is not a directory");
 		}
 		
@@ -40,8 +67,10 @@ public class File {
 	public Optional<File> getChild(String name) {
 		return childFiles.stream().filter(file -> file.name.equals(name)).findAny();
 	}
-	
-	public boolean isFile() {
-		return "file".equals(type);
+
+	@Override
+	public int compareTo(File otherFile) {
+		return Long.compare(this.size, otherFile.size);
 	}
+
 }
