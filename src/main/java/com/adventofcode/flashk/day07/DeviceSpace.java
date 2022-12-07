0b564ed6c43d19cd2e4c6519363386c3939879c0
Flashk <mevengar@gmail.com>
2022-12-07 09:20:27 +0100

    Refactor day 07 code a little bit

--- src/main/java/com/adventofcode/flashk/day07/DeviceSpace.java
+++ src/main/java/com/adventofcode/flashk/day07/DeviceSpace.java
@@ -3,110 +3,105 @@ package com.adventofcode.flashk.day07;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DeviceSpace {
 
-	private final static String CD_REGEX = "[$] (cd) ([\\/.a-z]*)";
+	private final static String CD_REGEX = "\\$ cd ([\\/.a-z]*)";
 	private final static Pattern CD_PATTERN = Pattern.compile(CD_REGEX);
-	
-	private final static String DIR_REGEX = "dir ([a-z]*)";
-	private final static Pattern DIR_PATTERN = Pattern.compile(DIR_REGEX);
-	
-	private final static String FILE_REGEX = "^^([0-9]*) ([a-z]*.[a-z]*)";
-	private final static Pattern FILE_PATTERN = Pattern.compile(FILE_REGEX);
 	
 	private final static String LS_COMMAND = "$ ls";
 	private final static String PARENT_DIR = "..";
 	
+	private final static int FILTER_FOLDER_MAX_SPACE = 100000;
 	private final static long TOTAL_SPACE = 70000000;
-	private final static long MINIMUM_SPACE = 30000000;
+	private final static long UPDATE_SIZE = 30000000;
 	
 	private File filesystem;
+	private long occupiedSpace;
 	private List<File> directories = new ArrayList<>();
-	
-	private long totalSpacePart1 = 0;
+
 	
 	public DeviceSpace(List<String> inputs) {
 		
+		initializeFileTree(inputs);
+		occupiedSpace = calculateSize(filesystem);
+		
+	}
+
+	private void initializeFileTree(List<String> inputs) {
 		Stack<File> currentDirectory = new Stack<>();
 		
 		for(String input : inputs) {
 			
 			Matcher matcher = CD_PATTERN.matcher(input);
 			
+			// cd <dirname>
 			if(matcher.find()) {
 			
-				// cd command match
-				String name = matcher.group(2);
+				String name = matcher.group(1);
 				
 				if(filesystem == null) {
 					
-					// Is root directory
-					filesystem = new File("dir", name, 0);
+					// Change dir to root directory
+					filesystem = new File(true, name, 0);
 					currentDirectory.add(filesystem);
+					directories.add(filesystem);
 					
 				} else if (!PARENT_DIR.equals(name)){
 					
-					// Is any other directory - add folder to stack
+					// Change dir to any other directory
 					File currentDir = currentDirectory.peek().getChild(name).get();
 					currentDirectory.add(currentDir);
 					directories.add(currentDir);
 				
 				} else {
 					
-					// Returns to parent directory
+					// Change dir to parent directory (..)
 					currentDirectory.pop();
 				}
 				
-				
 			} else if(!LS_COMMAND.equals(input)) {
 				
-				File currentDir = currentDirectory.peek();
-				currentDir.addChild(getFile(input));
+				// Add file or directory to current directory
+				currentDirectory.peek().addChild(new File(input));
 				
 			}
 			
 		}
-
 	}
 
 	public long solveA() {
-		calculateSize(filesystem);
 		
-		return this.totalSpacePart1;
+		AtomicLong totalSize = new AtomicLong(0);
+		directories.stream()
+					.filter(directory -> directory.getSize() <= FILTER_FOLDER_MAX_SPACE)
+					.forEach(directory -> totalSize.getAndAdd(directory.getSize()));
+		
+		return totalSize.get();
+		
 	}
 	
 	public long solveB() {
 		
-
-		long unusedSpace = TOTAL_SPACE - calculateSize(filesystem);
-		long neededSpace = MINIMUM_SPACE - unusedSpace;
-	
-		long currentMaxSpace = Long.MAX_VALUE;
-		for(File directory : directories) {
-			long directorySize = directory.getSize();
-			if(directorySize < currentMaxSpace && directorySize > neededSpace) {
-				currentMaxSpace = directorySize;
-			}
-		}
+		long unusedSpace = TOTAL_SPACE - occupiedSpace;
+		long neededSpace = UPDATE_SIZE - unusedSpace;
 		
-		return currentMaxSpace;
-		/*
 		return directories.stream()
 							.sorted()
 							.filter(directory -> directory.getSize() > neededSpace)
 							.findAny()
 							.get()
 							.getSize();
-							*/
+
 	}
 	
 	private long calculateSize(File currentFile) {
 		
-		// Caso base, hemos llegado a un fichero
-		if(currentFile.isFile()) {
+		// Base case: current file is a plain file (not directory)
+		if(!currentFile.isDirectory()) {
 			return currentFile.getSize();
 		}
 		
@@ -115,27 +110,8 @@ public class DeviceSpace {
 			currentSize += calculateSize(childFile);
 			currentFile.setSize(currentSize);
 		}
-		
-		if(currentSize <= 100000) {
-			this.totalSpacePart1 += currentSize;
-		}
-		
+
 		return currentSize;
 	}
 
-	private File getFile(String input) {
-
-		Matcher directoryMatcher = DIR_PATTERN.matcher(input);
-		Matcher fileMatcher = FILE_PATTERN.matcher(input);
-		
-		File file = null;
-		if(directoryMatcher.find()) {
-			file = new File("dir", directoryMatcher.group(1), 0);
-		} else if(fileMatcher.find()) {
-			long size = Long.parseLong(fileMatcher.group(1));
-			String filename = fileMatcher.group(2);
-			file = new File("file", filename, size);
-		}
-		return file;
-	}
 }
