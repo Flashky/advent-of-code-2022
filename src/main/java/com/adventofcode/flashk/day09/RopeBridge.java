0b25641404d3605c52829f9502e31c6e59c8140d
Flashk <mevengar@gmail.com>
2022-12-11 16:57:17 +0100

    Refactor day 09 puzzle nad improve Vector2 class

--- src/main/java/com/adventofcode/flashk/day09/RopeBridge.java
+++ src/main/java/com/adventofcode/flashk/day09/RopeBridge.java
@@ -9,16 +9,7 @@ import com.adventofcode.flashk.common.Vector2;
 
 public class RopeBridge {
 
-	// Constant vectors to avoid build the same vectors again
-	
-	private static final Vector2 RIGHT = Vector2.right();
-	private static final Vector2 LEFT = Vector2.left();
-	private static final Vector2 UP = Vector2.up();
-	private static final Vector2 DOWN = Vector2.down();
-	private static final Vector2 UP_RIGHT = Vector2.upRight();
-	private static final Vector2 UP_LEFT = Vector2.upLeft();
-	private static final Vector2 DOWN_RIGHT = Vector2.downRight();
-	private static final Vector2 DOWN_LEFT = Vector2.downLeft();
+
 	
 	private Set<Vector2> passedCoordinates = new HashSet<>();
 	
@@ -27,12 +18,6 @@ public class RopeBridge {
 	
 	private List<Movement> movements = new ArrayList<>();
 
-	private Vector2 diagonalDirectionA = new Vector2(1,2);
-	private Vector2 diagonalDirectionB = new Vector2(2,1);
-	private Vector2 diagonalDirectionC = new Vector2(2,2);
-	
-	
-	
 	public RopeBridge(List<String> inputs) {
 		
 		for(String input : inputs) {
@@ -89,9 +74,7 @@ public class RopeBridge {
 		Vector2 head = tailKnot.getNext().getPos();
 		Vector2 tail = tailKnot.getPos();
 		
-		/*
 		int distance = (int) Vector2.distance(head, tail);
-		
 
 		if(distance > 1) {
 			
@@ -116,79 +99,7 @@ public class RopeBridge {
 				passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
 			}
 		}
-		*/
-
 		
-		Vector2 distance = Vector2.substractAbs(head, tail);
-		if((distance.getX() > 1) && (distance.getY() == 0)) {
-			
-			distance.normalize();
-			//tail.transform(distance);
-			
-			// Left & Right
-			
-			if(head.getX() > tail.getX()) {
-				tail.transform(RIGHT);
-			} else {
-				tail.transform(LEFT);
-			}
-			
-			if(tailKnot.isLast()) {
-				passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
-			}
-			
-			
-		} else if((distance.getX() == 0) && (distance.getY() > 1)) {
-			
-			// Up & Down
-			
-			if(head.getY() > tail.getY()) {
-				tail.transform(UP);
-			} else {
-				tail.transform(DOWN);
-			}
-
-			if(tailKnot.isLast()) {
-				passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
-			}
-			
-		} else if(distance.equals(diagonalDirectionA) || distance.equals(diagonalDirectionB) || distance.equals(diagonalDirectionC)) {
-			
-			if(head.getY() > tail.getY()) {
-				
-				// Diagonal up
-				
-				if(head.getX() > tail.getX()) {
-					tail.transform(UP_RIGHT);
-					if(tailKnot.isLast()) {
-						passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
-					}
-				} else if (head.getX() < tail.getX()) {
-					tail.transform(UP_LEFT);
-					if(tailKnot.isLast()) {
-						passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
-					}
-				}
-				
-			} else if(head.getY() < tail.getY()) {
-
-				// Diagonal down
-				
-				if(head.getX() > tail.getX()) {
-					tail.transform(DOWN_RIGHT);
-					if(tailKnot.isLast()) {
-						passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
-					}
-				} else if (head.getX() < tail.getX()) {
-					tail.transform(DOWN_LEFT);
-					if(tailKnot.isLast()) {
-						passedCoordinates.add(new Vector2(tail.getX(), tail.getY()));
-					}
-				}
-				
-			}
-			
-		}
 	}
 
 }
