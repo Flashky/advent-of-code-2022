d4060f4ffc2e20af82520ec243df8f6f37ffcbec
Flashk <mevengar@gmail.com>
2022-12-11 18:26:40 +0100

    Refactor day 09 puzzle to use double linked list and recursion to perform movements

--- src/main/java/com/adventofcode/flashk/day09/Knot.java
+++ src/main/java/com/adventofcode/flashk/day09/Knot.java
@@ -4,20 +4,54 @@ import com.adventofcode.flashk.common.Vector2;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @ToString
 public class Knot {
 
 	private Vector2 pos = new Vector2();
 	private Knot next;
-	private boolean isLast = true;
+	private Knot prev;
 	
 	public Knot(Knot next) {
 		this.next = next;
-		next.isLast = false;
 	}
-	
+
+	/** 
+	 * Recursively applies a movement on the current knot and its sucessors.
+	 * @param direction
+	 * @return
+	 */
+	public Vector2 move(Vector2 direction) {
+		
+		if(prev == null) {
+			// Head knot
+			pos.transform(direction);
+		} else {
+			// Any tail knots
+			Vector2 head = prev.getPos();
+			Vector2 tail = this.getPos();
+			int distance = (int) Vector2.distance(head, tail);
+
+			if(distance > 1) {
+				
+				// Override head direction with tail direction
+				direction = Vector2.substract(head, tail);
+				direction.normalize();
+				tail.transform(direction);
+			}
+
+		}
+		
+		if(next == null) {
+			// Tail - Finished movement
+			return new Vector2(pos);
+		} else {
+			return next.move(direction);
+		}
+	}
 }
