b2e5c915c514d4793f38892b1989839a76fa1dfe
Flashk <mevengar@gmail.com>
2022-12-25 21:09:24 +0100

    Day 17 part 2 WIP

--- src/main/java/com/adventofcode/flashk/day17/PyroclasticFlow.java
+++ src/main/java/com/adventofcode/flashk/day17/PyroclasticFlow.java
@@ -1,10 +1,10 @@
 package com.adventofcode.flashk.day17;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.adventofcode.flashk.common.Collider2DL;
 import com.adventofcode.flashk.common.Vector2L;
@@ -35,7 +35,9 @@ public class PyroclasticFlow {
 	private Set<Collider2DL> colliders = new HashSet<>();
 	
 	// Part 2
-	//private Set<Vector2L> min
+	private TetrisPattern currentTetrisPattern;
+	private List<TetrisPattern> tetrisFullPattern = new ArrayList<>();
+	
 	
 	public PyroclasticFlow(String input) {
 		jetMovements = input.toCharArray();
@@ -53,27 +55,61 @@ public class PyroclasticFlow {
 	
 	/*
 	 Problemas a tratar:
-	 
-	 [x] Gestión de rocas
-	 	[x] Creación de rocas de todas las formas respecto a una posición (x,y) ya calculada.
-	 	[x] Movimiento de roca según un vector dirección.
-	 	[x] Cálculo de colisiones contra otras rocas o bordes del mapa.
-	 
-	 [x] Spawneo de rocas:
-	 	[x] Calcular la posición inicial (x,y) de la roca
-	 		[x] Coordenada x: 2 unidades respecto a la izquierda (x = 2)
-	 		[x] Coordenada y: 3 unidades respecto al collider que esté en la posición más alta:
-	 			[x] Caso inicial: el único collider que cuenta es colliderBottom situado en y = 0
-	 			[x] Caso tras primera roca: collider con y más alta entre todos los colliders de todas las rocas.
-	 	[x] Creación de la roca que corresponda según una lista circular.
-	 
-	 [x] Movimiento:
-	 	[x] Cálculo de vector dirección para el jet stream
-	 	[x] Gestor de alternancia de movimiento lateral - hacia abajo.
-	 	[x] Comprobación de colisión al hacer movimiento lateral.
-	 	[x] Comprobación de colisión al hacer movimiento vertical.
-	 
 
+	Parte 2
+	
+	Necesitaremos ver si se repite algún patrón. 
+	Una forma de visualizarlo es como una cadena.
+	
+	Por ejemplo ``ABCDXABCDX`` sería un patrón ``ABCDX`` que se repite dos veces.
+	
+	Pero ojo, puede haber patrones dentro de patrones:
+	1. ``ABCDABCDXABCDABCDX``, tenemos un patrón ABCDABCD que se repite dos veces.
+	2. De repente, aparece una ``X`` y el patrón se rompe.
+	3. Finalmente el patrón se ve que es ``ABCDABCDX`` 
+	
+	Para el tetris necesitaríamos una estructura de datos que nos permita identificar un estado único.
+
+	QUIERO una estructura de datos que permita modelar un estado
+	PARA poder guardar un listado de estados y comprobar si en algún momento se repite
+	
+	¿Ahora bien, qué es un estado?
+	El problema tiene claramente un patrón que se repite continuamente: las formas de las rocas, dando un nombre a cada una:
+	- A: Barra horizontal
+	- B: Cruz
+	- C: L
+	- D: Barra vertical
+	- E: cuadado
+	
+	Vemos que el tipo de roca no es suficiente para definir un estado, continuamente se repite un ciclo: "ABCDE ABCDE ABCDE..."
+	Por lo tanto, para definir un estado hace falta algo más. 
+	
+	Opciones:
+	
+	1. La coordenada horizontal en la que termina la roca:
+		1.1 El campo de juego va desde las coordenadas x=1 hasta x=7. Por lo que podemos guardar el nombre de la roca y su coordenada:
+			A,1 -> Barra horizontal que ha caido en la coordenada x=1
+			B,6 -> Cruz que ha caído en la coordenada x=6
+			
+	2. En el caso de que lo anterior no sea suficiente, podríamos guardar también el listado de movimientos que ha hecho dicha figura. Por ejemplo:
+		A,1,>>< -> Barra horizontal que ha caído en la coordenada x=1 después de moverse dos veces a la derecha y una vez a la izquierda
+		
+	Cuantas más restricciones pongamos, más fácil será evitar que el patrón se repita por casualidad.
+	
+	Por lo tanto, la estructura de datos podría ser un objeto que guarde los tres datos:
+	- Shape: String / Character
+	- Position: int
+	- Movements: String
+	
+	La estructura necesitaría un equals para comparar con otra estructura.
+	
+	Ahora nos falta otra estructura:.
+	
+	QUIERO una estructura que almacene un listado de patrones
+	PARA poder comparar con el patrón actual
+	
+	
+	
 		// Parte 2
 
 		[ ] Calcular para cada columna cuál es su y más baja.
@@ -115,16 +151,9 @@ public class PyroclasticFlow {
 						}
 					}
 					
-					/*
-					if(direction.equals(LEFT)) {
-						System.out.println("Push rock left -"+ nextRock.getPosition());
-					} else {
-						System.out.println("Push rock right - "+ nextRock.getPosition());
-					}*/
-					
-					
 					// Finished movement
 					// Next movement will be vertical
+					
 					jetGas = false;
 				} else {
 					
@@ -155,65 +184,13 @@ public class PyroclasticFlow {
 				maxY = nextRock.getMaxY();
 			}
 			
-			// TODO la solución de borrar colliders no es suficiente para un volumen de datos tan alto
-			// La solución va por buscar un patrón que se repita
-			// ¿Cómo almacenamos un patrón? ¿cómo lo comparamos? al principio no hay nada con lo que comparar.
-			//cleanColliderPool();
+			// Add current rock to pattern list
+			currentTetrisPattern.setX(nextRock.getPosition().getX());
+			System.out.println(currentTetrisPattern);
+			tetrisFullPattern.add(currentTetrisPattern);
 		}
 		
 		return maxY;
-		
-	}
-
-	private void cleanColliderPool() {
-		
-		boolean found = false;
-		long y = minY;
-		
-		Set<Vector2L> collisionPoints = new HashSet<>(); // TODO igual esto debería empezar en la última barra de bloqueo siempre
-		// Inicialmente la barra de bloqueo se empieza a evaluar para y = 0
-		// cuando se encuentre un bloqueo, se puede actualizar para y = y barra bloqueo
-		
-		collisionPoints.add(new Vector2L(1,y));
-		collisionPoints.add(new Vector2L(2,y));
-		collisionPoints.add(new Vector2L(3,y));
-		collisionPoints.add(new Vector2L(4,y));
-		collisionPoints.add(new Vector2L(5,y));
-		collisionPoints.add(new Vector2L(6,y));
-		collisionPoints.add(new Vector2L(7,y));
-		
-		while(!found && y < maxY) {
-			
-			Iterator<Vector2L> pointIterator = collisionPoints.iterator();
-			int collisionCount = 0;
-			while(collisionCount < 7 && pointIterator.hasNext()) {
-				
-				Vector2L point = pointIterator.next();
-				Optional<Collider2DL> collider = colliders.stream().filter(c -> c.collidesWith(point)).findAny();
-				
-				if(collider.isPresent()) {
-					collisionCount++;
-				}
-			}
-			
-			// Si tras haber recorrido todos los puntos, todos tenían colisión:
-			if(collisionCount == 7) {
-				
-				found = true;
-				maxY = y;
-				
-				// TODO Borrar todos los colliders por debajo de y - 5
-				long cutOffsetY = y - 5;
-				colliders = colliders.stream().filter(c -> c.getMinY() > cutOffsetY).collect(Collectors.toSet());
-			} else {
-				// Pasamos a evaluar la siguiente fila
-				y++;
-				collisionPoints.stream().forEach(p -> p.transformY(1));
-			}
-		
-		}
-		// Cuidado: los colliders del lateral y de abajo no se deben borrar.
-		// Comparar colliders
 		
 	}
 
@@ -229,6 +206,9 @@ public class PyroclasticFlow {
 		
 		// Obtain next jet movement
 		Vector2L direction = jetMovements[nextJetMovementIndex] == JET_LEFT ? LEFT : RIGHT;
+		
+		// Add movement to tetris pattern
+		currentTetrisPattern.addMovement(jetMovements[nextJetMovementIndex]);
 		
 		// Obtain next circular index
 		nextJetMovementIndex = (nextJetMovementIndex+1) % jetMovements.length;
@@ -257,6 +237,10 @@ public class PyroclasticFlow {
 				throw new IllegalArgumentException("Invalid rock type: "+rockShapeOrder[nextRockIndex]);
 		}
 	
+		// Save current pattern
+		currentTetrisPattern = new TetrisPattern();	
+		currentTetrisPattern.setShape(rockShapeOrder[nextRockIndex]);
+		
 		// Obtain next circular index
 		nextRockIndex = (nextRockIndex+1) % rockShapeOrder.length;
 		
