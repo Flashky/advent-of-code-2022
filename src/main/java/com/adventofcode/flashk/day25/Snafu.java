0d9345d8a10e03703e3d85c1ad1ea44294b2b530
Flashk <mevengar@gmail.com>
2022-12-25 20:04:11 +0100

    Minor refactor to day 25

--- src/main/java/com/adventofcode/flashk/day25/Snafu.java
+++ src/main/java/com/adventofcode/flashk/day25/Snafu.java
@@ -2,15 +2,17 @@ package com.adventofcode.flashk.day25;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Snafu {
 
 	
 	private static Map<Character, Long> decimalMap = Map.of(
-					'0', 0L,
-					'1', 1L,
-					'2', 2L,
-					'-', -1L,
-					'=', -2L);
+			'0', 0L,
+			'1', 1L,
+			'2', 2L,
+			'-', -1L,
+			'=', -2L);
 	
 	private static Map<Long, Character> snafuMap = Map.of(
 			0L, '0',
@@ -35,20 +37,16 @@ public class Snafu {
 	public static String snafuValue(long number) {
 		
 		if(number == 0) {
-			return ""; // Or 0, but it is a left side 0
+			return StringUtils.EMPTY; // Or 0, but it is a left side 0
 		}
 		
 		long mod = number % 5;
-		
-		Character snafuDigit = snafuMap.get(mod);
-		String nextSnafuDigit = "";
+
 		if(mod < 3) {
-			nextSnafuDigit = snafuValue(number / 5);
+			return snafuValue(number / 5) + snafuMap.get(mod);
 		} else {
-			nextSnafuDigit = snafuValue(number / 5 + 1);
+			return snafuValue(number / 5 + 1) + snafuMap.get(mod);
 		}
-		
-		return nextSnafuDigit + snafuDigit;
 		
 		
 	}
